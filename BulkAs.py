88924e88143f33d195c83cbf3dbb8e6f55826002
Dare <johndoe@example.com>
2023-07-04 15:25:01 +0300

    Json Loads and Parse

--- BulkAs.py
+++ BulkAs.py
@@ -1,25 +1,16 @@
 import json
 
-pets = {
-    'name': 'Charlie',
-    'age': 15,
-    'meals': ['Purina', 'Hills'],
-    'owner': {'fname': 'Bill', 'sname': 'Gates'}
-}
+with open('pets.json') as pet_file:
+    string = pet_file.read()
+    data = json.loads(string)
 
-with open('pets.json', 'w') as pet_file:
-    json.dump(pets, pet_file)
+for item in data:
+    if type(data[item]) == list:
+        print(item, ', ' .join(data[item]))
+    elif type(data[item]) == dict:
+        for k, v in data[item].items():
+            print(k, v)
+    else:
+        print(item, data[item])
 
-data = json.dumps(pets)
 
-# weekdays = ['Понедельник', 'Вторник','Среда','Четверг','Пятница', 'Суббота','Воскресенье']
-#
-# st = [i for i in range(1, 6)]
-# print(st)
-# week_dict = {key: val for key, val in enumerate(weekdays)}
-#
-# print(week_dict)
-#
-# data = json.dumps(week_dict)
-#
-# print(data)
