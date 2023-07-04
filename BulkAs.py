7dab0729221a137cafb53d6c567690f99be8a279
Dare <johndoe@example.com>
2023-07-04 14:51:53 +0300

    Json Dumps

--- BulkAs.py
+++ BulkAs.py
@@ -1,1 +1,13 @@
-# будет
+import json
+
+weekdays = ['Понедельник', 'Вторник','Среда','Четверг','Пятница', 'Суббота','Воскресенье']
+
+st = [i for i in range(1, 6)]
+print(st)
+week_dict = {key: val for key, val in enumerate(weekdays)}
+
+print(week_dict)
+
+data = json.dumps(week_dict)
+
+print(data)
