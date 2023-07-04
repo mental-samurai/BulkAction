8104c078cca4cb3fe4d13206c7a524874d81e327
Dare <johndoe@example.com>
2023-07-04 16:49:43 +0300

    Json weathe

--- BulkAs.py
+++ BulkAs.py
@@ -1,15 +1,24 @@
-import json
+import requests
+from datetime import datetime
 
-with open('pets.json') as pet_file:
-    data = json.load(pet_file)
+town = input('Введите город: ')
+#print(result)
 
-for item in data:
-    if type(data[item]) == list:
-        print(item, ', ' .join(data[item]))
-    elif type(data[item]) == dict:
-        for k, v in data[item].items():
-            print(k, v)
-    else:
-        print(item, data[item])
+key = 'ecc157672d5737c76936a7f7171672de'
+url = 'http://api.openweathermap.org/data/2.5/weather'
+params = {'APPID': key, 'q': 'town', 'units': 'metric'}
 
+request = requests.get(url, params=params)
 
+result = request.json()
+print(result['main']['fells_like'])
+code = result['cod']
+
+print(f'Температура: {data["temp"]:.1f}\xB0C')
+print('Влажность', data['humidity'], '%')
+raw_time_sunset = result['sys']['sunset']
+print(datetime.utcfromtimestamp(raw_time_sunset).strftime("%H:%M"))
+print(f'Координаты:{result["coord"]["lon"]}, {result["coord"]["lat"]}')
+print(result['weather'][0]['main'])
+if result['weather'][0]['main'] == 'Rain':
+    print('Дождь')
