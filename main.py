ad100ba9c64060f406a98191ff624500b36e7d08
vasya <vasya.454@gmail.com>
2023-07-10 11:12:15 +0300

    flask

---
+++ main.py
@@ -0,0 +1,17 @@
+# ('/other') - от корня (root)
+# ('./other') - от текущей директории
+# ('../other') - выйти из текущей директории
+# ('other') - в корне (на данном уровне с main.py)
+# ('../../other')
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return 'Salam'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
